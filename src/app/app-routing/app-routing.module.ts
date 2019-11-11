b4ee4341ea36da1086450ce15e56ddd01b0c2573
Nicolas Munar <n.munar@uniandes.edu.co>
2019-11-11 14:23:50 -0500

    Cambios

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -43,35 +43,6 @@ const routes: Routes = [
 
     ]
 },
-
-    {
-        path: 'estudiantes',
-        children:[
-            {
-                path:'list',
-                component: EstudianteListComponent
-            },
-            /*{
-                path:':id',
-                component: EstudianteDetailComponent,
-                outlet: 'detail'
-            }*/
-        ]
-    },
-    {
-        path: 'calificaciones',
-        children:[
-            {
-                path:'list',
-                component: CalificacionesListComponent
-            },
-            /*{
-                path:':id',
-                component:CalificaionDetailComponent,
-                outlet: 'detail'
-            }*/
-        ]
-    },
     {
       path: "home",
       component: HomeComponent
