619725061a6b6925f2af6da3ed6be535d2dc0ed0
Nicolas Munar <n.munar@uniandes.edu.co>
2019-11-11 23:49:58 -0500

    CAmbios pipe

--- src/app/oferta/filter.pipe.ts
+++ src/app/oferta/filter.pipe.ts
@@ -8,11 +8,5 @@ export class FilterPipe implements PipeTransform {
 
   transform(value: String, args?: String): any {
 
-    // Remove the duplicate elements
-    let uniqueArray = value.filter(function (el, index, array) { 
-      return array.indexOf (el) == index;
-    });
-
-    return uniqueArray;
   }
 }
