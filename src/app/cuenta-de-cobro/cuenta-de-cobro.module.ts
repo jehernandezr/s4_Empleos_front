7124e35622eb39797ef885b1e4f841ca58263de8
Jonatan Esmits Hernandez Rubio <je.hernandezr@uniandes.edu.co>
2019-11-11 23:03:50 -0500

    no genere error

--- src/app/cuenta-de-cobro/cuenta-de-cobro.module.ts
+++ src/app/cuenta-de-cobro/cuenta-de-cobro.module.ts
@@ -6,13 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CuentaDeCobroService } from './cuenta-de-cobro.service';
+import { CuentaDeCobroCreateComponent } from './cuenta-de-cobro-create/cuenta-de-cobro-create.component';
 
 @NgModule({
   imports: [
     CommonModule,HttpClientModule, AppRoutingModule, FormsModule,ReactiveFormsModule
   ],
-  declarations: [CuentaDeCobroListComponent,CuentaDeCobroDetailComponent],
-  exports:[CuentaDeCobroListComponent,CuentaDeCobroDetailComponent],
+  declarations: [CuentaDeCobroListComponent,CuentaDeCobroDetailComponent, CuentaDeCobroCreateComponent],
+  exports:[CuentaDeCobroListComponent,CuentaDeCobroDetailComponent,CuentaDeCobroCreateComponent],
   providers:[CuentaDeCobroService]
 })
 export class CuentaDeCobroModule { }
