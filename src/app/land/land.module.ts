196b6553c536f7e01b15d381f7b58cc1c3735dcd
David Dominguez <ld.dominguez@uniandes.edu.co>
2019-12-02 19:09:30 -0500

    Se arreglaron bugs menores

--- src/app/land/land.module.ts
+++ src/app/land/land.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LandComponent } from './land-component/land.component';
 import { LandEstudianteComponent } from "./land-estudiante/land-estudiante.component";
 import { LandContratistaComponent } from "./land-contratista/land-contratista.component";
+import { OfertaModule } from '../oferta/oferta.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    OfertaModule
   ],
   declarations: [ LandComponent, LandEstudianteComponent, LandContratistaComponent ],
 })
